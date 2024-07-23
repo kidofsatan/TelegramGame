5da1f52af3273a494cd8930046236d56688ab55d
kidofsatan <sergey_satanov@mail.ru>
2024-07-23 13:19:41 +0300

    Add project files.

---
+++ TelegramGame/UpgradesWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TelegramGame
+{
+    /// <summary>
+    /// Interaction logic for UpgradesWindow.xaml
+    /// </summary>
+    public partial class UpgradesWindow : Window
+    {
+        public UpgradesWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
